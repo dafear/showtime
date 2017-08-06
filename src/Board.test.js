96187585512cdc43c2a9cbbd0815ec74b6381d5a
sme_admin <dhafir.fuller@gmail.com>
2017-08-06 19:39:55 -0400

    fixed

--- src/Board.test.js
+++ src/Board.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Board from './componets/board';
+import Board from './components/board';
 import {shallow, mount} from 'enzyme';
 import {Provider} from 'react-redux';
 import store from './store';
--- src/List.test.js
+++ src/List.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import List from './componets/list';
+import List from './components/list';
 import {shallow, mount} from 'enzyme';
 
 
